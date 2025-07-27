e4392c635b257bc0b999969a281d813e6ecb84be
Nahiyanx <nahiyanx6@gmail.com>
2025-07-27 12:27:50 +0600

    Made a few minor changes

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,15 +1,17 @@
- <div class="p-4 bg-white/10 rounded-xl flex flex-col text-center">
-    <div class="self-start text-sm">Laracasts</div>
-    <div class="py-6 font-bold">
-        <h3>Video Producer</h3>
-        <p>Full Time - From $60,000</p>
-    </div>        
-    <div class="flex justify-between items-center mt-auto">
-        <div>
-            <a href="#" class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">Tag</a>
-            <a href="#" class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">Tag</a>
-            <a href="#" class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">Tag</a>
-        </div>
-        <img src="https://placehold.co/42x42" alt="" class="rounded-xl">
-    </div>
-</div>
+@props(['job'])
+
+<x-panel class="p-4 flex flex-col text-center">
+            <div class="self-start text-sm text-blue-200">{{$job->employer->name}}</div>
+            <div class="py-8">
+                <h3 class="group-hover:text-blue-500 text-xl font-bold transition-colors duration-400">{{$job->title}}</h3>
+                <p class="text-sm mt-4 text-gray-300">{{ $job->schedule }} - {{$job->salary}}</p>
+            </div>
+            <div class="flex justify-between items-center mt-auto">
+                <div>
+                    @foreach($job->tags as $tag)
+                        <x-tag :$tag size="small"></x-tag>
+                    @endforeach
+                </div>
+                <x-employer-logo :width="42"/>
+            </div>
+</x-panel>
